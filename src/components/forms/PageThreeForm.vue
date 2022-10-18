a38104a0a4461404a0e0d10b05d818d81cd2246d
holmista <tornikebuchukuri@redberry.ge>
2022-10-18 11:21:47 +0400

    feat: antibodies selection option

--- src/components/forms/PageThreeForm.vue
+++ src/components/forms/PageThreeForm.vue
@@ -1,13 +1,15 @@
 <template>
   <ValidationForm>
     <HadCovidSelect />
+    <AntibodiesSelect visible="yes" />
   </ValidationForm>
 </template>
 <script>
 import { Form as ValidationForm } from "vee-validate";
 import HadCovidSelect from "../form_elements/HadCovidSelect.vue";
+import AntibodiesSelect from "../form_elements/AntibodiesSelect.vue";
 
 export default {
-  components: { ValidationForm, HadCovidSelect },
+  components: { ValidationForm, HadCovidSelect, AntibodiesSelect },
 };
 </script>
