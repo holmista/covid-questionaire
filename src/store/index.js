c39cb5b98d24413e27952e89d20ddaf7bdf4beba
holmista <tornikebuchukuri@redberry.ge>
2022-10-23 20:15:11 +0400

    feat: page five state

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import { createStore } from "vuex";
 import basicInformationModule from "./modules/basicInformation";
 import covidInformationModule from "./modules/covidInformation";
 import vaccinationInformationModule from "./modules/vaccinationInformation";
+import officeInformationModule from "./modules/officeInformation";
 
 const store = createStore({
   modules: {
     basicInformation: basicInformationModule,
     covidInformation: covidInformationModule,
     vaccinationInformation: vaccinationInformationModule,
+    officeInformation: officeInformationModule,
   },
 });
 
